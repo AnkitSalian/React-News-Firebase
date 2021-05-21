bd771dc45082977a21558bc4046c3890431cd9b7
Ankit Salian <ankitsalian9@gmail.com>
2021-05-21 09:41:52 +0530

    Configured Header to route to different pages using NavLink

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import ForgotPassword from "./Auth/ForgotPassword";
+import Header from './Header';
 import Login from "./Auth/Login";
 import CreateLink from './Link/CreateLink';
 import SearchLinks from './Link/SearchLinks';
@@ -10,17 +11,22 @@ import LinkList from './Link/LinkList';
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/create" component={CreateLink} />
-        <Route path="/login" component={Login} />
-        <Route path="/forgotPassword" component={ForgotPassword} />
-        <Route path="/search" component={SearchLinks} />
-        <Route path="/top" component={LinkList} />
-        <Route path="/new/:page" component={LinkList} />
-        <Route path="/search" component={SearchLinks} />
-        <Route path="/link/:linkId" component={LinkDetail} />
-        <Route exact path="/" render={() => <Redirect to="new/1" />} />
-      </Switch>
+      <div className="app-container">
+        <Header />
+        <div className="route-container">
+          <Switch>
+            <Route path="/create" component={CreateLink} />
+            <Route path="/login" component={Login} />
+            <Route path="/forgotPassword" component={ForgotPassword} />
+            <Route path="/search" component={SearchLinks} />
+            <Route path="/top" component={LinkList} />
+            <Route path="/new/:page" component={LinkList} />
+            <Route path="/search" component={SearchLinks} />
+            <Route path="/link/:linkId" component={LinkDetail} />
+            <Route exact path="/" render={() => <Redirect to="new/1" />} />
+          </Switch>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
