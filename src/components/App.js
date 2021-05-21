24f15302d05c2a967a778b2faafa2453ec728905
Ankit Salian <ankitsalian9@gmail.com>
2021-05-21 09:19:40 +0530

    Configured page routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import ForgotPassword from "./Auth/ForgotPassword";
+import Login from "./Auth/Login";
+import CreateLink from './Link/CreateLink';
+import SearchLinks from './Link/SearchLinks';
+import LinkDetail from './Link/LinkDetail';
+import LinkList from './Link/LinkList';
 
 function App() {
   return (
-    <div style={{ margin: "0 auto", textAlign: "center" }}>
-      <img style={{ height: "100vh" }} src="/logo.png" alt="Logo" />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/create" component={CreateLink} />
+        <Route path="/login" component={Login} />
+        <Route path="/forgotPassword" component={ForgotPassword} />
+        <Route path="/search" component={SearchLinks} />
+        <Route path="/top" component={LinkList} />
+        <Route path="/new/:page" component={LinkList} />
+        <Route path="/search" component={SearchLinks} />
+        <Route path="/link/:linkId" component={LinkDetail} />
+        <Route exact path="/" render={() => <Redirect to="new/1" />} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
