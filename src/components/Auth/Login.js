fa7b77d634fce23aff9fd6126d73df65e1c7ef42
Ankit Salian <ankitsalian9@gmail.com>
2021-05-23 12:22:35 +0530

    Adding a reusable custom form hook

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,22 +1,41 @@
 import React, { useState } from "react";
+import useFormValidation from "./useFormValidation";
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: ""
+}
 
 function Login(props) {
 
+  const { handleChange, handleSubmit, values } = useFormValidation(INITIAL_STATE);
+
   const [login, setLogin] = useState(true);
+
   return <div>
     <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
-    <form className="flex flex-column">
+    <form onSubmit={handleSubmit} className="flex flex-column">
       {!login && <input
+        onChange={handleChange}
+        value={values.name}
+        name="name"
         type="text"
         placeholder="Name"
         autoComplete="off"
       />}
       <input
+        onChange={handleChange}
+        value={values.email}
+        name="email"
         type="email"
         placeholder="Email"
         autoComplete="off"
       />
       <input
+        onChange={handleChange}
+        value={values.password}
+        name="password"
         type="password"
         placeholder="Password"
       />
