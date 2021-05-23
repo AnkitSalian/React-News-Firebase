c68974735ad063775dfcb10f4c3cd3c93bc015c5
Ankit Salian <ankitsalian9@gmail.com>
2021-05-23 10:45:12 +0530

    Added toggling between Login and CreateAccount page

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login(props) {
-  return <div>Login</div>;
+
+  const [login, setLogin] = useState(true);
+  return <div>
+    <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
+    <form className="flex flex-column">
+      {!login && <input
+        type="text"
+        placeholder="Name"
+        autoComplete="off"
+      />}
+      <input
+        type="email"
+        placeholder="Email"
+        autoComplete="off"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+      />
+      <div className="flex mt3">
+        <button type="submit" className="button pointer mr2">
+          Submit
+        </button>
+        <button type="button" className="pointer button"
+          onClick={() => setLogin(prevLogin => !prevLogin)}
+        >
+          {login ? "Create Account" : "Already have an account?"}
+        </button>
+      </div>
+    </form>
+  </div>;
 }
 
 export default Login;
