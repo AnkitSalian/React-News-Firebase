06ba60c3e63f3072c167c5256de5880820c1b3eb
Ankit Salian <ankitsalian9@gmail.com>
2021-05-27 09:50:01 +0530

    Added Create link with validation

--- src/components/Auth/validateCreateLink.js
+++ src/components/Auth/validateCreateLink.js
@@ -1,1 +1,17 @@
-export default function validateCreateLink(values) {}
+export default function validateCreateLink(values) {
+    let errors = {};
+
+    if (!values.description) {
+        errors.description = "Description Required";
+    } else if (values.description.length < 10) {
+        errors.description = "Description must be atleast 10 characters";
+    }
+
+    if (!values.url) {
+        errors.url = "url Required";
+    } else if (!/^(ftp|http|https):\/\/[^ "]+$/.test(values.url)) {
+        errors.url = "Invalid URL";
+    }
+
+    return errors;
+}
