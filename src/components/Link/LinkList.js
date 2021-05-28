58e53e80e873129a43896f784bb3e80f26aea20b
Ankit Salian <ankitsalian9@gmail.com>
2021-05-28 09:11:29 +0530

    Fetched published table data from firebase using onSnapShot

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,6 +1,26 @@
 import React from "react";
+import { FirebaseContext } from "../../firebase";
 
 function LinkList(props) {
+
+  const { firebase } = React.useContext(FirebaseContext);
+
+  React.useEffect(() => {
+    getLinks();
+  }, []);
+
+  function getLinks() {
+    firebase.db.collection('Links').onSnapshot(handleSnapshot);
+  }
+
+  function handleSnapshot(snapshot) {
+    const links = snapshot.docs.map(doc => {
+      return { id: doc.id, ...doc.data() }
+    });
+
+    console.log({ links });
+  }
+
   return <div>LinkList</div>;
 }
 
